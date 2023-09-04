242910ea1849b59b4b107ffcc7196fd523ce5b27
RozanAlawar <114680942+RozanAlawar@users.noreply.github.com>
2023-09-04 18:49:39 +0300

    initial commit

---
+++ lib/injection_container.dart
@@ -0,0 +1,60 @@
+import 'package:get_it/get_it.dart';
+import 'package:dio/dio.dart';
+
+import 'core/utils/navigation.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  // Dio
+  Dio client = Dio(
+      // BaseOptions(
+      //   receiveDataWhenStatusError: true,
+      //   connectTimeout:
+      //       const Duration(milliseconds: Endpoints.connectionTimeout),
+      //   receiveTimeout: const Duration(milliseconds: Endpoints.receiveTimeout),
+      //   baseUrl: Endpoints.baseUrl,
+      // ),
+      );
+
+  sl.registerLazySingleton<Dio>(() => client);
+
+  // Dependencies
+
+  // Services
+  sl.registerSingleton<NavigationService>(
+    NavigationService(),
+  );
+
+  // sl.registerSingleton<NewsApiService>(NewsApiService(sl()));
+
+  // sl.registerSingleton<ArticleRepository>(
+  //   ArticleRepositoryImpl(sl(),sl())
+  // );
+
+  //UseCases
+  // sl.registerSingleton<GetArticleUseCase>(
+  //   GetArticleUseCase(sl())
+  // );
+
+  // sl.registerSingleton<GetSavedArticleUseCase>(
+  //   GetSavedArticleUseCase(sl())
+  // );
+
+  // sl.registerSingleton<SaveArticleUseCase>(
+  //   SaveArticleUseCase(sl())
+  // );
+
+  // sl.registerSingleton<RemoveArticleUseCase>(
+  //   RemoveArticleUseCase(sl())
+  // );
+
+  //Blocs
+  // sl.registerFactory<RemoteArticlesBloc>(
+  //   ()=> RemoteArticlesBloc(sl())
+  // );
+
+  // sl.registerFactory<LocalArticleBloc>(
+  //   ()=> LocalArticleBloc(sl(),sl(),sl())
+  // );
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'config/theme/theme_manager.dart';
+import 'core/utils/navigation.dart';
+import 'injection_container.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Ensure that ScreenUtil's screen size is initialized.
+  await ScreenUtil.ensureScreenSize();
+
+  // Initialize dependency injection
+  await initializeDependencies();
+
+  // Load app configuration (splash data).
+  // sl<AppConfig>().loadData();
+
+  // Set preferred device orientations and system UI overlay style.
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.black,
+      statusBarBrightness: Brightness.dark,
+    ),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Dakakeen',
+          theme: appTheme(),
+          navigatorKey: sl<NavigationService>().navigatorKey,
+          scaffoldMessengerKey: sl<NavigationService>().snackBarKey,
+          // initialRoute: Routes.splash,
+          // onGenerateRoute: RouterX.generateRoute,
+        );
+      },
+    );
+  }
+}
