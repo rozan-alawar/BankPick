d1abb754453d6377ba7263757d4c11c29e0a860d
Rozan <rosanalawer2002@gmail.com>
2024-07-14 09:54:51 +0300

    Update Send Money Screen

--- lib/presentation/card/send_money_screen.dart
+++ lib/presentation/card/send_money_screen.dart
@@ -28,12 +28,12 @@ class SendMoneyScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final homeProvider = Provider.of<HomeProvider>(context);
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.w,vertical: 20.h),
-        child: Column(
-          children: [
-            20.height,
-            Row(
+      body: Column(
+        children: [
+          20.height,
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+            child: Row(
               children: [
                 GestureDetector(
                   onTap: () => sl<NavigationService>().pop(),
@@ -54,26 +54,124 @@ class SendMoneyScreen extends StatelessWidget {
                 ),
               ],
             ),
-            40.height,
-            SizedBox(
-              height: 250.h,
-              width: double.infinity,
-              child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  // physics: BouncingScrollPhysics(),
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) => BuildCardSection(
-                        cardNumber: homeProvider.user!.cards[index].cardNumber,
-                        cardHolderName: homeProvider.user!.cards[index].cardHolder,
-                        expiryDate: homeProvider.user!.cards[index].expiryDate,
-                        cvv: homeProvider.user!.cards[index].cvv,
-                        cardType: homeProvider.user!.cards[index].cardType,
-                      ),
-                  separatorBuilder: (context, index) => 20.width,
-                  itemCount: homeProvider.user!.cards.length),
+          ),
+          40.height,
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: List<Widget>.generate(homeProvider.user!.cards.length,
+                  (index) {
+                return Padding(
+                  padding: EdgeInsetsDirectional.only(
+                      start: index == 0 ? 20.w : 6.w,
+                      end: index == homeProvider.user!.cards.length - 1
+                          ? 20.w
+                          : 6.w),
+                  child: Container(
+                    height: 240.h,
+                    width: 320.w,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: BuildCardSection(
+                      cardNumber: homeProvider.user!.cards[index].cardNumber,
+                      cardHolderName:
+                          homeProvider.user!.cards[index].cardHolder,
+                      expiryDate: homeProvider.user!.cards[index].expiryDate,
+                      cvv: homeProvider.user!.cards[index].cvv,
+                      cardType: homeProvider.user!.cards[index].cardType,
+                    ),
+                  ),
+                );
+              }),
             ),
-          ],
-        ),
+          ),
+          40.height,
+          Container(
+            height: 120.h,
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(horizontal: 20.w),
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 20.h),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25.r),
+              border: Border.all(
+                color: ColorManager.secondaryText,
+              ),
+              color: Colors.white.withOpacity(0.8),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                PrimaryText(
+                  'Send to',
+                  fontWeight: FontWeight.w500,
+                ),
+              ],
+            ),
+          ),
+          20.height,
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20.w),
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 30.h),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25.r),
+              border: Border.all(
+                color: ColorManager.secondaryText,
+              ),
+              color: Colors.white.withOpacity(0.8),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    PrimaryText(
+                      'Enter Your Amount',
+                      color: ColorManager.secondaryText,
+                      fontSize: 11.sp,
+                    ),
+                    Spacer(),
+                    PrimaryText(
+                      'Change Currency',
+                      color: Colors.red,
+                      fontSize: 11.sp,
+                    ),
+                  ],
+                ),
+                20.height,
+                Row(
+                  children: [
+                    PrimaryText(
+                      'USD',
+                      color: Colors.lightBlue,
+                      fontSize: 24.sp,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    16.width,
+                    PrimaryText(
+                      '2200.00',
+                      fontSize: 24.sp,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          40.height,
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: PrimaryButton(
+              color: ColorManager.primary,
+              onPressed: () => sl<NavigationService>().pop(),
+              child: PrimaryText(
+                'Send Money',
+                fontWeight: FontWeight.w600,
+                fontSize: 16.sp,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
