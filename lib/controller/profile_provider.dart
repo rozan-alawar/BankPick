f0a1ac6408eaf1900ebc6e9039f57023fd7a7fe6
Rozan <rosanalawer2002@gmail.com>
2024-07-24 10:26:53 +0300

    Refactor code

--- lib/controller/profile_provider.dart
+++ lib/controller/profile_provider.dart
@@ -9,30 +9,31 @@ import '../model/message_model.dart';
 import '../model/user_model.dart';
 
 class ProfileProvider with ChangeNotifier {
-   User_Model? user = CacheHelper.getData(key: 'user')??User_Model(
-    name: 'Tanya Myroniuk',
-    avatarUrl: ImageAssets.profile,
-    email: 'tanya.myroniuk@gmail.com',
-    phoneNumber: '+8801712663389',
-    birthDate: DateTime(2000, 9, 28),
-    joinedDate: DateTime(2021, 1, 28),
-    cards: [
-      CardModel(
-        cardNumber: '1234 5678 9876 5432',
-        cardHolder: 'Rozan AbuAlawar',
-        expiryDate: '09/25',
-        cvv: '123',
-        cardType: 'Visa',
-      ),
-      CardModel(
-        cardNumber: '4321 8765 6789 1234',
-        cardHolder: 'Tanya Myroniuk',
-        expiryDate: '11/24',
-        cvv: '456',
-        cardType: 'Mastercard',
-      ),
-    ],
-  );
+  User_Model? user = CacheHelper.getData(key: 'user') ??
+      User_Model(
+        name: 'Tanya Myroniuk',
+        avatarUrl: ImageAssets.profile,
+        email: 'tanya.myroniuk@gmail.com',
+        phoneNumber: '+8801712663389',
+        birthDate: DateTime(2000, 9, 28),
+        joinedDate: DateTime(2021, 1, 28),
+        cards: [
+          CardModel(
+            cardNumber: '1234 5678 9876 5432',
+            cardHolder: 'Rozan AbuAlawar',
+            expiryDate: '09/25',
+            cvv: '123',
+            cardType: 'Visa',
+          ),
+          CardModel(
+            cardNumber: '4321 8765 6789 1234',
+            cardHolder: 'Tanya Myroniuk',
+            expiryDate: '11/24',
+            cvv: '456',
+            cardType: 'Mastercard',
+          ),
+        ],
+      );
   void saveProfile(GlobalKey<FormState> formKey, BuildContext context,
       String name, String email, String phone) {
     if (formKey.currentState?.validate() ?? false) {
@@ -50,40 +51,64 @@ class ProfileProvider with ChangeNotifier {
       Navigator.pop(context);
     }
   }
-    List<Message> contactUsMessages = [
-      Message(
-        sender: User_Model(
-            name: 'Bank',
-            avatarUrl: ImageAssets.appLogoPng,
-            birthDate: DateTime.now(),
-            cards: [],
-            email: '',
-            joinedDate: DateTime.now(),
-            phoneNumber: ""),
-        body: "Hello! How can we assist you today?",
-        date: DateTime.now().subtract(const Duration(minutes: 5)),
-      ),
-      Message(
-        sender:  CacheHelper.getData(key: 'user'),
-        body: "I have a question about my recent transaction.",
-        date: DateTime.now().subtract(const Duration(minutes: 3)),
-      ),
-    ];
 
-    void sendMessage({
-      required TextEditingController controller,
-    }) {
-      if (controller.text.trim().isNotEmpty) {
-        contactUsMessages.add(
-          Message(
-            sender: user!,
-            body: controller.text.trim(),
-            date: DateTime.now(),
+  List<Message> contactUsMessages = [
+    Message(
+      sender: User_Model(
+          name: 'Bank',
+          avatarUrl: ImageAssets.appLogoPng,
+          birthDate: DateTime.now(),
+          cards: [],
+          email: '',
+          joinedDate: DateTime.now(),
+          phoneNumber: ""),
+      body: "Hello! How can we assist you today?",
+      date: DateTime.now().subtract(const Duration(minutes: 5)),
+    ),
+    Message(
+      sender: CacheHelper.getData(key: 'user') ??
+          User_Model(
+            name: 'Tanya Myroniuk',
+            avatarUrl: ImageAssets.profile,
+            email: 'tanya.myroniuk@gmail.com',
+            phoneNumber: '+8801712663389',
+            birthDate: DateTime(2000, 9, 28),
+            joinedDate: DateTime(2021, 1, 28),
+            cards: [
+              CardModel(
+                cardNumber: '1234 5678 9876 5432',
+                cardHolder: 'Rozan AbuAlawar',
+                expiryDate: '09/25',
+                cvv: '123',
+                cardType: 'Visa',
+              ),
+              CardModel(
+                cardNumber: '4321 8765 6789 1234',
+                cardHolder: 'Tanya Myroniuk',
+                expiryDate: '11/24',
+                cvv: '456',
+                cardType: 'Mastercard',
+              ),
+            ],
           ),
-        );
-        notifyListeners();
-        controller.clear();
-      }
+      body: "I have a question about my recent transaction.",
+      date: DateTime.now().subtract(const Duration(minutes: 3)),
+    ),
+  ];
+
+  void sendMessage({
+    required TextEditingController controller,
+  }) {
+    if (controller.text.trim().isNotEmpty) {
+      contactUsMessages.add(
+        Message(
+          sender: user!,
+          body: controller.text.trim(),
+          date: DateTime.now(),
+        ),
+      );
+      notifyListeners();
+      controller.clear();
     }
   }
-
+}
