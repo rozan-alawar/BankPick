5917f6416dfd1e0b523e93dc2680b7d8c26d2542
Rozan <rosanalawer2002@gmail.com>
2024-07-31 14:19:21 +0300

    REFACTOR CODE!

--- lib/presentation/profile/message_center_screen.dart
+++ lib/presentation/profile/message_center_screen.dart
@@ -24,60 +24,10 @@ class MessageCenterScreen extends StatefulWidget {
   State<MessageCenterScreen> createState() => _MessageCenterScreenState();
 }
 
-class _MessageCenterScreenState extends State<MessageCenterScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:  PrimaryAppBar(
-        title: LocaleKeys.message_center.tr(),
-        withLeading: true,
-      ),
-      body: ListView.separated(
-        separatorBuilder: (context, index) => 16.height,
-        itemCount: Message.messages.length,
-        itemBuilder: (context, index) {
-          final message = Message.messages[index];
-          return ListTile(
-            leading: Image.asset(ImageAssets.profile),
-            title: Text(message.sender.name),
-            subtitle: PrimaryText(
-              message.body,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              fontSize: 12.sp,
-            ),
-            trailing: Text(
-              "${message.date.day}/${message.date.month}/${message.date.year}",
-              style: const TextStyle(color: Colors.grey),
-            ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => MessageDetailScreen(message: message),
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-}
-
-class MessageDetailScreen extends StatefulWidget {
-  final Message message;
-
-  MessageDetailScreen({required this.message});
-
-  @override
-  State<MessageDetailScreen> createState() => _MessageDetailScreenState();
-}
-
 TextEditingController? messageController;
 GlobalKey<FormState>? formKey;
 
-class _MessageDetailScreenState extends State<MessageDetailScreen> {
+class _MessageCenterScreenState extends State<MessageCenterScreen> {
   @override
   void initState() {
     // TODO: implement initState
@@ -89,110 +39,75 @@ class _MessageDetailScreenState extends State<MessageDetailScreen> {
   @override
   Widget build(BuildContext context) {
     final profileProvider = Provider.of<ProfileProvider>(context);
-    bool isSentByUser =
-        widget.message.sender.name == profileProvider.user!.name;
 
     return Scaffold(
       appBar: PrimaryAppBar(
-        title: widget.message.sender.name,
+        title: LocaleKeys.message_center.tr(),
         withLeading: true,
       ),
       body: GestureDetector(
         onTap: () {
           FocusScope.of(context).unfocus();
         },
-        child: Padding(
-          padding:  EdgeInsets.symmetric(horizontal: 8.w),
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-
-                Align(
-                  alignment: isSentByUser
-                      ? Alignment.centerRight
-                      : Alignment.centerLeft,
-                  child: Container(
-                    margin: EdgeInsetsDirectional.only(
-                        end: isSentByUser?10.w:70.w,
-                        top: 5.h,
-                        start: isSentByUser ? 70.w : 10,
-                        bottom: 5.h),
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 10.0, horizontal: 15.0),
-                    decoration: BoxDecoration(
-                      color: isSentByUser ? Colors.blue : Colors.grey[300],
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: isSentByUser
-                          ? CrossAxisAlignment.end
-                          : CrossAxisAlignment.start,
-                      children: [
-                        PrimaryText(
-                          widget.message.body,
-                          maxLines: 8,
-                          fontSize: 14.sp,
-                          color: isSentByUser ? Colors.white : Colors.black,
-                        ),
-                        5.height,
-                        Text(
-                          "${widget.message.date.hour}:${widget.message.date.minute}",
-                          style: TextStyle(
-                            fontSize: 10,
-                            color:
-                                isSentByUser ? Colors.white60 : Colors.black54,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  reverse: true,
+                  itemCount: profileProvider.contactUsMessages.length,
+                  itemBuilder: (context, index) {
+                    final message = profileProvider.contactUsMessages[
+                        profileProvider.contactUsMessages.length - 1 - index];
+                    return MessageBubbleWidget(message: message);
+                  },
                 ),
-                const Spacer(),
-                Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 8.w, vertical: 20.h),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          controller: messageController,
-                          decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                color: ColorManager.secondaryText,
-                              ),
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(16.r),
-                              ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 20.h),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: messageController,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: ColorManager.secondaryText,
                             ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                color: ColorManager.secondaryText,
-                              ),
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(16.r),
-                              ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(16.r),
                             ),
                           ),
-                          validator: (value) {},
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: ColorManager.secondaryText,
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(16.r),
+                            ),
+                          ),
                         ),
+                        validator: (value) {
+                          return null;
+                        },
                       ),
-                      8.width,
-                      GestureDetector(
-                        onTap: () => profileProvider.sendMessage(
-                            controller: messageController!,withOtherUser: true,),
-                        child: const Icon(
-                          Icons.send_outlined,
-                          color: ColorManager.primary,
-                          size: 30,
-                        ),
+                    ),
+                    8.width,
+                    GestureDetector(
+                      onTap: () => profileProvider.sendMessage(
+                          controller: messageController!, withOtherUser: false),
+                      child: const Icon(
+                        Icons.send_outlined,
+                        color: ColorManager.primary,
+                        size: 30,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/presentation/settings/contact_us_screen.dart
+++ lib/presentation/settings/contact_us_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dakakeen/config/theme/assets_manager.dart';
 import 'package:dakakeen/config/theme/color_manager.dart';
 import 'package:dakakeen/core/extensions/empty_space_extension.dart';
 import 'package:dakakeen/core/lang/locale_keys.g.dart';
@@ -5,10 +6,12 @@ import 'package:dakakeen/presentation/profile/message_bubble_widget.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 import '../../controller/profile_provider.dart';
 import '../../core/common_widget/primary_appbar.dart';
+import '../../core/common_widget/primary_text.dart';
 
 class ContactUsScreen extends StatefulWidget {
   const ContactUsScreen({super.key});
@@ -17,100 +20,73 @@ class ContactUsScreen extends StatefulWidget {
   State<ContactUsScreen> createState() => _ContactUsScreenState();
 }
 
-TextEditingController? messageController;
-GlobalKey<FormState>? formKey;
-
 class _ContactUsScreenState extends State<ContactUsScreen> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    formKey = GlobalKey<FormState>();
-    messageController = TextEditingController();
-  }
-
   @override
   Widget build(BuildContext context) {
     final profileProvider = Provider.of<ProfileProvider>(context);
 
     return Scaffold(
-      appBar:  PrimaryAppBar(
+      appBar: PrimaryAppBar(
         title: LocaleKeys.contact_us.tr(),
         withLeading: true,
       ),
-      body: GestureDetector(
-        onTap: () {
-          FocusScope.of(context).unfocus();
-        },
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                  reverse: true,
-                  itemCount: profileProvider.contactUsMessages.length,
-                  itemBuilder: (context, index) {
-                    final message = profileProvider.contactUsMessages[
-                        profileProvider.contactUsMessages.length - 1 - index];
-                    return MessageBubbleWidget(message: message);
-                  },
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                SvgPicture.asset(
+                  IconAssets.email,
+                  width: 25.w,
+                  height: 25.h,
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 20.h),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextFormField(
-                        controller: messageController,
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide:  const BorderSide(
-                              color: ColorManager.secondaryText,
-                            ),
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(16.r),
-                            ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: ColorManager.secondaryText,
-                            ),
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(16.r),
-                            ),
-                          ),
-                        ),
-                        validator: (value) {
-                          return null;
-                        },
-                      ),
-                    ),
-                    8.width,
-                    GestureDetector(
-                      onTap: () => profileProvider.sendMessage(
-                          controller: messageController!, withOtherUser: false),
-                      child: const Icon(
-                        Icons.send_outlined,
-                        color: ColorManager.primary,
-                        size: 30,
-                      ),
-                    ),
-                  ],
+                8.width,
+                PrimaryText("Email",
+                    fontSize: 14.sp, color: ColorManager.secondaryText),
+              ],
+            ),
+            8.height,
+            PrimaryText(
+              "info@bankPick.ps",
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w600,
+            ),
+            25.height,
+            Row(
+              children: [
+                SvgPicture.asset(
+                  IconAssets.phone,
+                  width: 25.w,
+                  height: 25.h,
                 ),
-              ),
-            ],
-          ),
+                8.width,
+                PrimaryText("Phone Number",
+                    fontSize: 14.sp, color: ColorManager.secondaryText),
+              ],
+            ),
+            8.height,
+            PrimaryText(
+              "789456321",
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w600,
+            ),
+            30.height,
+            PrimaryText("You can find us on social media ",
+                fontSize: 14.sp, color: ColorManager.secondaryText),
+16.height,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.facebook,size: 40.h,),
+                Icon(Icons.telegram,size: 40.h,),
+                Icon(Icons.snapchat,size: 40.h,),
+              ],
+            ),
+          ],
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    messageController!.dispose();
-    super.dispose();
   }
 }
